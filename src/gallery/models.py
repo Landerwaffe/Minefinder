8ed7e6386eb4360a1e46a8e5ad9057eb1df0c59c
Saltyric <916545108@qq.com>
2022-03-26 17:11:13 +0800

    updated gallery models, remember to use command 'makemigrations' and 'migrate'

--- src/gallery/models.py
+++ src/gallery/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.utils.html import format_html
+
 
 # Create your models here.
+
+class Img(models.Model):
+    src = models.FileField(max_length=60, verbose_name='Location', upload_to='static/upload')
+    title = models.CharField(max_length=16, verbose_name='Title')
+
+    class Meta:
+        verbose_name_plural = 'Images Chart'
+
+    def __str__(self):
+        return self.title
+
+    def admin_image(self):
+        return format_html(
+            '<img src = "{}" width="200px"',
+            self.src.url
+        )
+
+    admin_image.short_description = u'Image'
