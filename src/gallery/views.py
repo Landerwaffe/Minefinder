6b3cf03c862a13669f0c8c474f94bea112dd764f
Saltyric <916545108@qq.com>
2022-03-26 17:36:58 +0800

    updated gallery image flood function updated gallery admin manage function

--- src/gallery/views.py
+++ src/gallery/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.http import JsonResponse
+from .models import Img
 
 
 # Create your views here.
 
 class Gallery(View):
     def get(self, request):
-        return render(request, 'gallery.html',{
-            'title':'Gallery',
-        })
+        img_list = Img.objects.all()
+        return render(request, 'gallery.html', {
+            'img_list': img_list,
+            'title': 'Gallery',
+        }, )
 
-    def post(self, request):
-        pass
+    def get_imgs(request):
+        nid = request.GET.get('nid')
+        img_list = Img.objects.filter(id__gt=nid).values('id', 'src', 'title').order_by('id')
+        img_list = list(img_list)
+        ret = {
+            'status': True,
+            'data': img_list
+        }
+        return JsonResponse(ret)
