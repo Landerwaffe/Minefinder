0b96e18edafec429caf3e3cb4d5d1933c942870a
Saltyric <916545108@qq.com>
2022-03-26 17:36:58 +0800

    updated gallery image flood function updated gallery admin manage function

--- src/gallery/admin.py
+++ src/gallery/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Img
+
 
 # Register your models here.
+@admin.register(Img)
+class ImgAdmin(admin.ModelAdmin):
+    list_display = ['id', 'src', 'title', 'admin_image']
+
+    search_fields = ['title']
+
+    list_per_page = 10
