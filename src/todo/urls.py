0f93bc30fee7495aef684fc28a9558ec4f8757a2
Landerwaffe <jonathan.daniel@connect.qut.edu.au>
2022-05-11 17:06:11 +0700

    Revert "Merge branch 'Ver-1'"
    This reverts commit 3dd719c22292c1bc902ff822549666de2d5d76e7, reversing
    changes made to 81eb5cc7fed52aa27a92861755d5284ff2e4a5ba.

--- src/todo/urls.py
+++ src/todo/urls.py
@@ -1,25 +1,40 @@
+"""todo URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
 from django.contrib import admin
-from django.urls import path, re_path, include
-from django.views.static import serve
+from django.urls import path
+from django.conf.urls import include
 from django.conf import settings
-from django.http import Http404
+from django.conf.urls.static import static 
 
-
-def http404(request):
-    raise Http404('404')
-
+# from tasks.views import *
+from board.views import *
+from tasks.views import *
+from chat.views import *
 
 
 def http404(request):
     raise Http404('404')
 
 urlpatterns = [
+    path('login/', login_view, name = "login"),
+    path('register/', registerPage, name = "register"),
     path('admin/', admin.site.urls),
-    path("", include("home.urls")),
-    # users
-    path("users/", include("users.urls")),
-    # ckeditor
-    path(r'ckeditor/', include('ckeditor_uploader.urls')),
+    path('card/', card_view, name="card"),
+    path('profile/', profile_view, name = 'profile'),
+    path('', splash_view, name = 'home'),
     path('chat/', include('chat.urls')),
     path('', include('faq.urls')),
     # media
@@ -54,3 +69,6 @@ urlpatterns = [
     # accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm']
     # accounts/reset/done/ [name='password_reset_complete']
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
