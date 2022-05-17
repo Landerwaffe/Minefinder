ad0de5f054cdd1deb976826124c1ee68d8b00274
Landerwaffe <jonathan.daniel@connect.qut.edu.au>
2022-05-17 12:14:20 +0700

    dynamic url test

--- src/todo/urls.py
+++ src/todo/urls.py
@@ -38,6 +38,8 @@ urlpatterns = [
     path('projectdetails/', details_view, name = 'projectdetails'),
     path('dealroom/', dealroom_view, name = 'dealroom'),
     path('', include('faq.urls')),
+    path('thread/', thread_view, name= 'thread'),
+    path('dynamic/<int:pk>',dynamic_view, name='dynamic'),
 
     path('formtest/', formtest_view, name='formtest')
     # Paths provided by django.contrib.auth.urls
