aae9367c9762d0b40fb679b9e3c6060603494b3c
Landerwaffe <jonathan.daniel@connect.qut.edu.au>
2022-05-28 15:16:18 +0700

    Crop image form

--- src/board/forms.py
+++ src/board/forms.py
@@ -8,6 +8,9 @@ from .models import *
 from .models import Customer
 from django.utils.translation import gettext_lazy as _
 
+from PIL import Image
+from django.core.files import File
+
 """
 Forms.py description
 --------------------
@@ -74,3 +77,28 @@ class ThreadForm(forms.ModelForm):
     class Meta:
         model = dealRoom
         fields = '__all__'
+
+class PhotoForm(forms.ModelForm):
+    x = forms.FloatField(widget=forms.HiddenInput())
+    y = forms.FloatField(widget=forms.HiddenInput())
+    width = forms.FloatField(widget=forms.HiddenInput())
+    height = forms.FloatField(widget=forms.HiddenInput())
+
+    class Meta:
+        model = Customer
+        fields = ('pfp', 'x', 'y', 'width', 'height', )
+
+    def save(self):
+        photo = super(PhotoForm, self).save()
+
+        x = self.cleaned_data.get('x')
+        y = self.cleaned_data.get('y')
+        w = self.cleaned_data.get('width')
+        h = self.cleaned_data.get('height')
+
+        image = Image.open(photo.file)
+        cropped_image = image.crop((x, y, w+x, h+y))
+        resized_image = cropped_image.resize((200, 200), Image.ANTIALIAS)
+        resized_image.save(photo.file.path)
+
+        return photo
